9d7b025014c92a2e2ac9488da263f917c1c00ed7
Diego Parra <dp@linkfire.com>
2023-11-14 16:55:05 -0400

    updates

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -8,4 +8,15 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn first_name(n: String) {
+    println!("First name: {}", n);
+}
+
+fn last_name(n: String) {
+    println!("Last name: {}", n);
+}
+
+fn main() {
+    first_name("Diego".to_string());
+    last_name("Parra".to_string());
+}
--- src/bin/a2.rs
+++ src/bin/a2.rs
@@ -8,4 +8,17 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+// * Use a function to add two numbers together
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// * Use a function to display the result
+fn display(n: i32) {
+    println!("{:?}", n);
+}
+
+fn main() {
+    let result = add(10, 10);
+    display(result);
+}
